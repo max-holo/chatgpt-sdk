bd77b04b66a67a4434d4d6a54c25d395067130bd
max-holo <maxuan07@126.com>
2024-08-12 14:37:50 +0800

    feat:通义千问：dashscope-http模式，同步接口修复

--- src/main/java/com/unfbx/chatgpt/interceptor/QwenParamInterceptor.java
+++ src/main/java/com/unfbx/chatgpt/interceptor/QwenParamInterceptor.java
@@ -2,11 +2,13 @@ package com.unfbx.chatgpt.interceptor;
 
 
 import cn.hutool.http.Header;
+import cn.hutool.json.JSONObject;
 import com.unfbx.chatgpt.function.AccessKeyHandler;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.Buffer;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -26,10 +28,20 @@ public class QwenParamInterceptor extends RequestParamInterceptor {
         Request original = chain.request();
         String url = original.url().toString();
         RequestBody body = original.body();
+
+        // 获取请求体内容
+        Buffer buffer = new Buffer();
+        body.writeTo(buffer);
+        String requestBodyString = buffer.readUtf8();
+
+        // 解析 JSON
+        JSONObject jsonObject = new JSONObject(requestBodyString);
+        boolean incrementalOutput = jsonObject.getJSONObject("parameters").getBool("incremental_output");
+
         Request request = original.newBuilder()
                 .url(url)
                 .header(Header.CONTENT_TYPE.getValue(), "application/json; charset=utf-8")
-                .header(Header.ACCEPT.getValue(), "text/event-stream")
+                .header(Header.ACCEPT.getValue(), incrementalOutput ? "text/event-stream" : "*/*")
                 .header(Header.AUTHORIZATION.getValue(), "Bearer " + accessKey)
                 .method(original.method(), body).build();
         return chain.proceed(request);
