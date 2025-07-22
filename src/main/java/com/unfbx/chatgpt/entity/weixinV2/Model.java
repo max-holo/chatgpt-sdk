6da5b5e7b1b343bae14d29bb5eb6fbf60d743531
max-holo <maxuan07@126.com>
2025-07-22 14:23:02 +0800

    feat:百度v2版本模型列表更新

--- src/main/java/com/unfbx/chatgpt/entity/weixinV2/Model.java
+++ src/main/java/com/unfbx/chatgpt/entity/weixinV2/Model.java
@@ -10,7 +10,9 @@ public enum Model {
 
     DEEPSEEK_V3("deepseek-chat", "自动为v3模型"),
     DEEPSEEK_R1("deepseek-r1", "深度思考模型"),
-    ERNIE_BOT_4("ernie-4.0-8k", "ernie-4.0-8k"),
+    ERNIE_BOT_4("ernie-4.0-8k", "ernie-4.0-8k模型"),
+    ERNIE_BOT_3_5("ernie-3.5-128k", "3.5-128k模型"),
+    ERNIE_BOT_4_5("ernie-4.5-turbo-128k", "4.5-128k模型"),
     ;
     private final String code;
     private final String info;
