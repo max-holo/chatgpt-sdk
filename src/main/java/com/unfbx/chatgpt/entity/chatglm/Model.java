d32162db1d29a3e3d3feb5cb2238189229073044
maxuan <maxuan@kttx.cn>
2026-03-12 09:37:54 +0800

    feat:增加GLM5模型

--- src/main/java/com/unfbx/chatgpt/entity/chatglm/Model.java
+++ src/main/java/com/unfbx/chatgpt/entity/chatglm/Model.java
@@ -25,6 +25,7 @@ public enum Model {
     GLM_4("glm-4","适用于复杂的对话交互和深度内容创作设计的场景"),
     GLM_4V("glm-4v","根据输入的自然语言指令和图像信息完成任务，推荐使用 SSE 或同步调用方式请求接口"),
     COGVIEW_3("cogview-3","根据用户的文字描述生成图像,使用同步调用方式请求接口"),
+    GLM_5("glm-5","适用于复杂的对话交互和深度内容创作设计的场景"),
     ;
     private final String code;
     private final String info;
